d7c1d6935ff52f0dc4f768e534350828ea6b0a1f
sarathkolassery <clicktosarath@gmail.com>
2024-04-09 16:09:13 +0530

    commit_1

---
+++ resources/views/ui-layouts/footer.blade.php
@@ -0,0 +1,60 @@
+<footer class="footer-half section-padding pb-0" style="margin-bottom: -127px;">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-5">
+          <div class="cont">
+            <div class="logo">
+              <a href="#0">
+                <img src="{{ asset('ui/img/cb-logo.png')}}" alt="coralbeachstudios logo" />
+              </a>
+            </div>
+            <div class="con-info custom-font">
+              <ul>
+                <li><span>Email : </span> coralbeachstudios@gmail.com</li>
+                <li>
+                  <span>Our Nexus : </span> Kozhikode, Kerala, India
+                </li>
+                <li><span>Phone : </span> +91 8136 8380 49,
+                  +91 7034 511 749</li>
+              </ul>
+            </div>
+            <div class="social-icon">
+              <h6 class="custom-font stit simple-btn">Follow Us</h6>
+              <div class="social">
+                <a href="https://www.facebook.com/coralbeachstudios" class="icon">
+                  <i class="fab fa-facebook-f"></i>
+                </a>
+                <a href="https://www.instagram.com/coralbeachstudios/" class="icon">
+                  <i class="fab fa-instagram"></i>
+                </a>
+                <a href="https://www.youtube.com/@roadbuddies9870" class="icon">
+                  <i class="fab fa-youtube"></i>
+                </a>
+                <a href="https://www.youtube.com/@bigmongerpromotions7933" class="icon">
+                  <i class="fab fa-youtube"></i>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-5 offset-lg-2">
+          <div class="subscribe mb-50">
+            <h6 class="custom-font stit simple-btn">QUICK ACCESS</h6>
+            <ul style="padding: 7px;">
+              <li><a href="index.html" class="nav-link">Home</a></li>
+              <li><a href="services.html" class="nav-link">Services</a></li>
+              <li><a href="about.html" class="nav-link">About</a></li>
+              <li><a href="portfolio.html" class="nav-link">Portfolio</a></li>
+              <li><a href="contact.html" class="nav-link">Contact</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="copyrights text-center">
+        <p>©
+          <script>document.write(new Date().getFullYear())</script>, coralbeach studios <a href="#0">All rights
+            reserved</a>.
+        </p>
+      </div>
+    </div>
+  </footer>
---
+++ resources/views/ui-layouts/header.blade.php
@@ -0,0 +1,60 @@
+<nav class="navbar change navbar-expand-lg" style="width: 100%;">
+    <!-- curve start -->
+    <!-- <svg id="header-svg" xmlns="http://www.w3.org/2000/svg" version="1.1" width="100%" height="60" viewBox="0 0 100 100"
+      preserveAspectRatio="none">
+      <path fill="%230099ff" fill-opacity="1" d="M0 50 C40 0 60 0 100 50 Z"></path>
+    </svg> -->
+    <!-- curve end -->
+    <div class="container nav-dy-width">
+      <!-- Logo -->
+      <a class="logo" href="#">
+        <img src="{{ asset('ui/img/cb-logo.png')}}" alt="coralbeachstudios logo"/>
+      </a>
+
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="icon-bar"><i class="fas fa-bars"></i></span>
+      </button>
+
+      <!-- navbar links -->
+      <div class="collapse navbar-collapse" id="navbarSupportedContent" style="margin-top: 10px;">
+        <ul class="navbar-nav ml-auto" style="padding-bottom: 15px;">
+          <li class="nav-item">
+            <a class="nav-link" href="index.html">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="services.html">Services</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="about.html">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="portfolio.html">Portfolio</a>
+          </li>
+          <!-- <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true"
+              aria-expanded="false">Portfolio</a>
+            <div class="dropdown-menu glass-drop">
+              <a class="dropdown-item" href="works.html">Mouse Info</a>
+              <a class="dropdown-item" href="works2.html">Masonry 3 Columns</a>
+              <a class="dropdown-item" href="works3.html">Masonry 2 Columns</a>
+              <a class="dropdown-item" href="works4.html">Pinterest List</a>
+            </div>
+          </li> -->
+          <li class="nav-item">
+            <a class="nav-link" href="contact.html">Contact</a>
+          </li>
+        </ul>
+        <!-- <div class="search">
+          <span class="icon pe-7s-search cursor-pointer"></span>
+          <div class="search-form text-center custom-font">
+            <form>
+              <input type="text" name="search" placeholder="Search" />
+            </form>
+            <span class="close pe-7s-close cursor-pointer"></span>
+          </div>
+        </div> -->
+      </div>
+    </div>
+    </div>
+  </nav>
