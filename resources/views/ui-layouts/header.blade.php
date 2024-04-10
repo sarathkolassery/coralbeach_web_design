2eadff384d1a2067fc102382caa07b1aeff45fb2
sarathkolassery <clicktosarath@gmail.com>
2024-04-10 17:23:39 +0530

    hgjgj

--- resources/views/ui-layouts/header.blade.php
+++ resources/views/ui-layouts/header.blade.php
@@ -23,7 +23,7 @@
             <a class="nav-link" href="{{url('/')}}">Home</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="{{url('/services')}}">Services2</a>
+            <a class="nav-link" href="{{url('/services')}}">Services</a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="{{url('/about')}}">About</a>
